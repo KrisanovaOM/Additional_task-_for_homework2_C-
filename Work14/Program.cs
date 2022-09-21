4b7cd326793f59b68391d011204b870d37282efc
KrisanovaOM <fentifluyshka@gmail.com>
2022-09-21 10:19:02 +0300

    Созданы папки Work14 и Work16

---
+++ Work14/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// Метод проверки кратности числа
+int IsMultiple(int number, int multiple)
+{
+  if (number % multiple == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+// Метод проверки одновременной кратности 7 и 23
+void BothMultiple7and23(int enterNumber)
+{
+  if (IsMultiple(enterNumber, 7) == 1 && IsMultiple(enterNumber, 23) == 1)
+  {
+    Console.WriteLine("Введенная цифра: " + enterNumber + " кратна одновременно и 7 и 23");    
+  }
+  else
+  {
+    Console.WriteLine("Введенная цифра: " + enterNumber + " не кратна одновременно и 7 и 23"); 
+  }
+}
+
+//Ввод проверяемой цифры
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Вызов методов
+BothMultiple7and23(number);
---
+++ Work16/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b || b == a*a) 
+  Console.WriteLine("Одно число является квадратом другого");
+else   Console.WriteLine("Ни одно из чисел не является квадратом другого"); 
